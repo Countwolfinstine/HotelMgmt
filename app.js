02d6a2e382c1244872d9e4c810500bf18d65dd4c
Naveen V Patil <naveenpatil26@gmail.com>
2017-10-20 23:28:17 +0530

    added the queues api

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-var express = require("express");
+ var express = require("express");
+var cors = require("cors");
 
 var app = express();
+
+var sk = [
+    {
+        term: "Rip",
+        food: "xyz",
+        defined: "To move at a high rate of speed"
+    },
+    {
+        term: "Huck",
+        food: "xyz",
+        defined: "To throw your body off of something, usually a natural feature like a cliff"
+    },
+    {
+        term: "Chowder",
+        food: "xyz",
+        defined: "Powder after it has been sufficiently skied"
+    }
+];
+
+
 
 app.use(function(req,res,next){
 	console.log(`${req.method} request for '${req.url}'`);
@@ -9,8 +30,15 @@ app.use(function(req,res,next){
 
 app.use(express.static("./"))
 
+app.use(cors());
+
+app.get("/queue-api",function(req,res){
+	res.json(sk);
+
+});
+
 app.listen(3000);
 
 console.log("Express running on port 3000");
 
-module.export = app;	
+module.export = app;	 
