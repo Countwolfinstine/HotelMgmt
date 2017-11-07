b22b1a73df5265f003a60f056f8a8777231a4e0f
Naveen V Patil <naveenpatil26@gmail.com>
2017-11-07 19:01:22 +0530

    Added the recommendation server-side code

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+var PythonShell = require('python-shell');
 var express = require("express");
+var bodyParser = require("body-parser")
 var cors = require("cors");
-var PythonShell = require('python-shell');
 var path = require("path");
-var PythonShell = require('python-shell');
+
+var app = express();
+
+
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "restaurantmgmt"
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 var options = {
     mode: 'json',
-   
 };
-var app = express();
 
 var sk = [
     {
@@ -30,7 +43,7 @@ var sk = [
 
 
 app.use(function(req,res,next){
-	console.log(`${req.method} request for '${req.url}'`);
+	console.log(`${req.method} request for '${req.url}' - $(JSON.stringify(req.body)}`);
 	next();
 });
 
@@ -38,15 +51,58 @@ app.use(express.static("./"))
 
 app.use(cors());
 
+con.connect();
 app.get("/queue-api",function(req,res){
-
-PythonShell.run('/scripts/Samplepython.py', options, function (err, results) {
-    if (err) throw err;
-    console.log(results);
-	res.send(results);
+    con.query("SELECT * FROM currentorder", function (err, result, fields) {
+        if (err) throw err;
+        res.send(result);
+    });
 
 });
 
+app.post("/queue-api", function(req, res){
+    console.log('gg');
+});
+
+app.delete("/queue-api/:term", function(req,res){
+   console.log("gg");
+   con.query("SELECT * FROM currentorder", function (err, result, fields) {
+        if (err) throw err;
+        res.send(result);
+    });
+});
+
+
+
+app.get("/menu-display/:usserid",function(req,res){
+    
+    var shell = new PythonShell('/RecommenderSystem/src/Recommender.py', { mode: 'json'});
+    console.log("yolo1");
+    
+    con.query("SELECT * from currentorder", function(err,results,fields){
+        console.log(results);
+        console.log("yolo2");
+        if(err) throw err;
+        console.log(results);
+        shell.send(results);
+        console.log("yolo3");
+        shell.on('message', function (message) {
+            // received a message sent from the Python script (a simple "print" statement)
+            console.log(message);
+        });
+        // end the input stream and allow the process to exit
+        shell.end(function (err) {
+            if (err) throw err;
+            console.log('finished');
+        });
+    
+    });
+    
+    con.query("SELECT * FROM currentorder", function (err, result, fields) {
+        if (err) throw err;
+        res.send(result);
+    });
+    
 });
 
 app.listen(3000);
