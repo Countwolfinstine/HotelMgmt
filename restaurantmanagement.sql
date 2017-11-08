3e8dd3a9438ba1656a49a5d2c46d3a4f0c0876a4
Nishanth Nakshatri <nishanths96@gmail.com>
2017-11-08 21:48:09 +0530

    updated db schema with data
    two significant changes made - 
    1. added quantity and time in 'orderlog' table so that it maintains the complete log required for further analysis
    2. time is also included in 'primary key' because the same user can order multiple items during the same timestamp for the same order id.
    
    feedback and the two ingredients table are yet to be filled.

---
+++ restaurantmanagement.sql
@@ -0,0 +1,448 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 08, 2017 at 11:47 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `restaurantmanagement`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `currentorder`
+--
+
+CREATE TABLE `currentorder` (
+  `order_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `currentorder`
+--
+
+INSERT INTO `currentorder` (`order_id`, `item_id`) VALUES
+(2, 5),
+(5, 5),
+(8, 10),
+(11, 10),
+(14, 5),
+(17, 3),
+(20, 4),
+(23, 13),
+(26, 10),
+(29, 2),
+(32, 4),
+(35, 3),
+(38, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `userid` int(11) NOT NULL,
+  `feedback` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ingredients`
+--
+
+CREATE TABLE `ingredients` (
+  `ing_id` int(11) NOT NULL,
+  `ing_name` varchar(30) NOT NULL,
+  `quantity` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ingredients_required`
+--
+
+CREATE TABLE `ingredients_required` (
+  `ing_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `ing_quantity_required` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `item_name` varchar(20) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`item_name`, `item_id`, `price`) VALUES
+('Dosa', 1, 25),
+('Idli', 2, 20),
+('Vada', 3, 20),
+('Biriyani', 4, 40),
+('Dal Makhani', 5, 50),
+('Dal', 6, 20),
+('Mixed Veg', 7, 50),
+('Aalo Gobhi', 8, 40),
+('Pav Bhaji', 9, 30),
+('Paalak Paneer', 10, 40),
+('Chole-Bhature', 11, 50),
+('Puri', 12, 20),
+('Naan', 13, 25),
+('Roti', 14, 25);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `tableid` int(11) NOT NULL,
+  `orderid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `itemid` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`tableid`, `orderid`, `userid`, `itemid`, `quantity`, `time`) VALUES
+(4, 1, 15, 1, 1, '2017-09-08 11:44:18'),
+(2, 2, 36, 5, 4, '2017-09-08 04:22:40'),
+(3, 3, 17, 1, 4, '2017-09-08 15:25:13'),
+(1, 4, 40, 13, 3, '2017-09-08 16:42:07'),
+(1, 5, 47, 5, 2, '2017-09-08 04:09:18'),
+(3, 6, 16, 4, 3, '2017-09-08 02:00:31'),
+(3, 7, 46, 1, 1, '2017-09-08 06:44:04'),
+(3, 8, 29, 10, 2, '2017-09-08 13:42:20'),
+(3, 9, 11, 7, 1, '2017-09-08 14:24:25'),
+(4, 10, 21, 8, 3, '2017-09-08 10:22:31'),
+(2, 11, 14, 10, 3, '2017-09-08 10:01:35'),
+(1, 12, 19, 12, 1, '2017-09-08 07:54:52'),
+(3, 13, 24, 9, 3, '2017-09-08 04:14:12'),
+(1, 14, 25, 5, 1, '2017-09-08 15:02:51'),
+(3, 15, 32, 4, 1, '2017-09-08 08:50:58'),
+(2, 16, 23, 11, 1, '2017-09-08 12:48:46'),
+(3, 17, 45, 3, 4, '2017-09-08 15:49:09'),
+(4, 18, 35, 12, 3, '2017-09-08 02:49:15'),
+(3, 19, 39, 6, 1, '2017-09-08 05:31:37'),
+(4, 20, 5, 4, 1, '2017-09-08 07:49:04'),
+(1, 21, 11, 14, 2, '2017-09-08 08:43:46'),
+(1, 22, 23, 6, 2, '2017-09-08 05:44:49'),
+(2, 23, 25, 13, 1, '2017-09-08 09:57:34'),
+(4, 24, 12, 4, 4, '2017-09-08 06:00:55'),
+(3, 25, 27, 2, 4, '2017-09-08 02:22:09'),
+(3, 26, 26, 10, 2, '2017-09-08 16:44:02'),
+(4, 27, 14, 6, 2, '2017-09-08 03:14:23'),
+(2, 28, 14, 9, 1, '2017-09-08 13:26:02'),
+(3, 29, 39, 2, 4, '2017-09-08 05:47:03'),
+(3, 30, 44, 1, 2, '2017-09-08 05:23:34'),
+(3, 31, 43, 4, 2, '2017-09-08 02:56:02'),
+(2, 32, 5, 4, 1, '2017-09-08 16:32:32'),
+(3, 33, 41, 7, 2, '2017-09-08 10:38:07'),
+(4, 34, 43, 8, 4, '2017-09-08 14:51:37'),
+(3, 35, 33, 3, 2, '2017-09-08 02:19:27'),
+(2, 36, 37, 7, 4, '2017-09-08 11:16:57'),
+(4, 37, 38, 9, 4, '2017-09-08 03:46:40'),
+(3, 38, 32, 4, 4, '2017-09-08 14:22:56'),
+(2, 39, 33, 2, 4, '2017-09-08 07:51:32'),
+(2, 40, 47, 8, 3, '2017-09-08 08:21:30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderlog`
+--
+
+CREATE TABLE `orderlog` (
+  `orderid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `itemid` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orderlog`
+--
+
+INSERT INTO `orderlog` (`orderid`, `userid`, `itemid`, `quantity`, `time`) VALUES
+(1, 15, 1, 1, '2017-09-08 11:44:18'),
+(2, 36, 5, 4, '2017-09-08 04:22:40'),
+(3, 17, 1, 4, '2017-09-08 15:25:13'),
+(4, 40, 13, 3, '2017-09-08 16:42:07'),
+(5, 47, 5, 2, '2017-09-08 04:09:18'),
+(6, 16, 4, 3, '2017-09-08 02:00:31'),
+(7, 46, 1, 1, '2017-09-08 06:44:04'),
+(8, 29, 10, 2, '2017-09-08 13:42:20'),
+(9, 11, 7, 1, '2017-09-08 14:24:25'),
+(10, 21, 8, 3, '2017-09-08 10:22:31'),
+(11, 14, 10, 3, '2017-09-08 10:01:35'),
+(12, 19, 12, 1, '2017-09-08 07:54:52'),
+(13, 24, 9, 3, '2017-09-08 04:14:12'),
+(14, 25, 5, 1, '2017-09-08 15:02:51'),
+(15, 32, 4, 1, '2017-09-08 08:50:58'),
+(16, 23, 11, 1, '2017-09-08 12:48:46'),
+(17, 45, 3, 4, '2017-09-08 15:49:09'),
+(18, 35, 12, 3, '2017-09-08 02:49:15'),
+(19, 39, 6, 1, '2017-09-08 05:31:37'),
+(20, 5, 4, 1, '2017-09-08 07:49:04'),
+(21, 11, 14, 2, '2017-09-08 08:43:46'),
+(22, 23, 6, 2, '2017-09-08 05:44:49'),
+(23, 25, 13, 1, '2017-09-08 09:57:34'),
+(24, 12, 4, 4, '2017-09-08 06:00:55'),
+(25, 27, 2, 4, '2017-09-08 02:22:09'),
+(26, 26, 10, 2, '2017-09-08 16:44:02'),
+(27, 14, 6, 2, '2017-09-08 03:14:23'),
+(28, 14, 9, 1, '2017-09-08 13:26:02'),
+(29, 39, 2, 4, '2017-09-08 05:47:03'),
+(30, 44, 1, 2, '2017-09-08 05:23:34'),
+(31, 43, 4, 2, '2017-09-08 02:56:02'),
+(32, 5, 4, 1, '2017-09-08 16:32:32'),
+(33, 41, 7, 2, '2017-09-08 10:38:07'),
+(34, 43, 8, 4, '2017-09-08 14:51:37'),
+(35, 33, 3, 2, '2017-09-08 02:19:27'),
+(36, 37, 7, 4, '2017-09-08 11:16:57'),
+(37, 38, 9, 4, '2017-09-08 03:46:40'),
+(38, 32, 4, 4, '2017-09-08 14:22:56'),
+(39, 33, 2, 4, '2017-09-08 07:51:32'),
+(40, 47, 8, 3, '2017-09-08 08:21:30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+CREATE TABLE `rating` (
+  `userid` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rating`
+--
+
+INSERT INTO `rating` (`userid`, `item_id`, `rating`) VALUES
+(5, 4, 3),
+(11, 7, 2),
+(11, 14, 4),
+(12, 4, 4),
+(14, 6, 2),
+(14, 9, 1),
+(14, 10, 2),
+(15, 1, 2),
+(16, 4, 1),
+(17, 1, 2),
+(19, 12, 1),
+(21, 8, 4),
+(23, 6, 3),
+(23, 11, 2),
+(24, 9, 1),
+(25, 5, 1),
+(25, 13, 4),
+(26, 10, 3),
+(27, 2, 4),
+(29, 10, 3),
+(32, 4, 1),
+(35, 12, 1),
+(36, 5, 2),
+(39, 2, 4),
+(39, 6, 2),
+(40, 13, 4),
+(43, 4, 1),
+(44, 1, 2),
+(45, 3, 4),
+(46, 1, 1),
+(47, 5, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `emailid` varchar(40) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `fname` varchar(20) NOT NULL,
+  `lname` varchar(20) NOT NULL,
+  `autorization` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`emailid`, `userid`, `password`, `fname`, `lname`, `autorization`) VALUES
+('Surya.Shankar93@live.com', 1, 'WnqIcnlA', 'Surya', 'Shankar', 'Waiter'),
+('Rajesh.Kumar15@hotmail.com', 2, 'ZmfiPvBZ', 'Rajesh', 'Kumar', 'Waiter'),
+('Ram.Jain50@hotmail.com', 3, 'RoJUZXW', 'Ram', 'Jain', 'Waiter'),
+('Surya.Jain88@hotmail.com', 4, 'IrbrkKiOY', 'Surya', 'Jain', 'Customer'),
+('Ram.Kumar80@live.com', 5, 'fGFNQtX', 'Ram', 'Kumar', 'Customer'),
+('Sam.Prasad75@live.com', 6, 'ONzTaFAjn', 'Sam', 'Prasad', 'Waiter'),
+('Ramesh.John80@gmail.com', 7, 'QSxJkcrT', 'Ramesh', 'John', 'Waiter'),
+('Tom.Prasad13@hotmail.com', 8, 'YBbILjBB', 'Tom', 'Prasad', 'Waiter'),
+('Rajesh.Shankar92@yahoo.com', 9, 'RFDEwhXO', 'Rajesh', 'Shankar', 'Customer'),
+('Ramesh.John69@hotmail.com', 10, 'vqjFAjJwI', 'Ramesh', 'John', 'Waiter'),
+('Rajesh.Jain78@gmail.com', 11, 'hTxJfNgzR', 'Rajesh', 'Jain', 'Customer'),
+('Ramesh.Iyer23@live.com', 12, 'txMYMRViY', 'Ramesh', 'Iyer', 'Customer'),
+('Sam.Shankar68@yahoo.com', 13, 'gozBpLcc', 'Sam', 'Shankar', 'Waiter'),
+('Tom.Prasad43@gmail.com', 14, 'zSFJalPzZ', 'Tom', 'Prasad', 'Customer'),
+('Tom.Jain23@yahoo.com', 15, 'xOdIqTh', 'Tom', 'Jain', 'Customer'),
+('Sham.Shankar9@yahoo.com', 16, 'sbgaGCxR', 'Sham', 'Shankar', 'Customer'),
+('Rajesh.Iyer32@yahoo.com', 17, 'SeWuCfLN', 'Rajesh', 'Iyer', 'Customer'),
+('Sham.Jain65@yahoo.com', 18, 'qQfvzicU', 'Sham', 'Jain', 'Customer'),
+('Ram.Shankar78@gmail.com', 19, 'blAKCXC', 'Ram', 'Shankar', 'Customer'),
+('Ram.Prasad41@gmail.com', 20, 'cEZYoiFA', 'Ram', 'Prasad', 'Customer'),
+('Sam.Iyer94@yahoo.com', 21, 'AsGWREZ', 'Sam', 'Iyer', 'Customer'),
+('Ram.Shankar2@live.com', 22, 'ikhMAHuo', 'Ram', 'Shankar', 'Customer'),
+('Rajesh.Kumar84@yahoo.com', 23, 'ZtsfVlL', 'Rajesh', 'Kumar', 'Customer'),
+('Sham.John15@hotmail.com', 24, 'QJQiYHB', 'Sham', 'John', 'Customer'),
+('Rajesh.Jain36@live.com', 25, 'fTEfGft', 'Rajesh', 'Jain', 'Customer'),
+('Rajesh.Iyer93@hotmail.com', 26, 'jjhaoOnK', 'Rajesh', 'Iyer', 'Customer'),
+('Sham.Shankar75@live.com', 27, 'mDhyTmxrj', 'Sham', 'Shankar', 'Customer'),
+('Tom.Iyer55@yahoo.com', 28, 'WrvDPzCel', 'Tom', 'Iyer', 'Customer'),
+('Rajesh.Iyer64@live.com', 29, 'QKBVgzqb', 'Rajesh', 'Iyer', 'Customer'),
+('Rajesh.Prasad57@live.com', 30, 'gScyVrNJR', 'Rajesh', 'Prasad', 'Customer'),
+('Ramesh.John3@yahoo.com', 31, 'JPmauFRVW', 'Ramesh', 'John', 'Customer'),
+('Sham.Shankar98@hotmail.com', 32, 'zjnAxyff', 'Sham', 'Shankar', 'Customer'),
+('Sham.John16@hotmail.com', 33, 'XncLJfZX', 'Sham', 'John', 'Customer'),
+('Sam.Prasad3@yahoo.com', 34, 'nRVnKfx', 'Sam', 'Prasad', 'Customer'),
+('Rajesh.Prasad47@yahoo.com', 35, 'UXjeCNY', 'Rajesh', 'Prasad', 'Customer'),
+('Ramesh.Shankar71@gmail.com', 36, 'KIbAFFdk', 'Ramesh', 'Shankar', 'Customer'),
+('Tom.Bhat6@gmail.com', 37, 'hBcgIFCvT', 'Tom', 'Bhat', 'Customer'),
+('Sham.Jain6@hotmail.com', 38, 'GDPGoAO', 'Sham', 'Jain', 'Customer'),
+('Tom.John18@yahoo.com', 39, 'WqukViqCN', 'Tom', 'John', 'Customer'),
+('Sam.Shankar98@yahoo.com', 40, 'XurVXHFm', 'Sam', 'Shankar', 'Customer'),
+('Ram.John62@live.com', 41, 'xqxSTJV', 'Ram', 'John', 'Customer'),
+('Sham.John76@live.com', 42, 'gwuqaYeH', 'Sham', 'John', 'Customer'),
+('Rajesh.Iyer87@yahoo.com', 43, 'HVNaYiw', 'Rajesh', 'Iyer', 'Customer'),
+('Ram.Prasad30@live.com', 44, 'fyBrVKvU', 'Ram', 'Prasad', 'Customer'),
+('Ramesh.Shankar22@gmail.com', 45, 'qXUQOXqEI', 'Ramesh', 'Shankar', 'Customer'),
+('Tom.Jain29@live.com', 46, 'UtmaAWq', 'Tom', 'Jain', 'Customer'),
+('Surya.John51@gmail.com', 47, 'yegIdCIW', 'Surya', 'John', 'Customer'),
+('Surya.Prasad96@yahoo.com', 48, 'YThshkdA', 'Surya', 'Prasad', 'Customer'),
+('Surya.Bhat83@gmail.com', 49, 'ZbfWmrQM', 'Surya', 'Bhat', 'Customer'),
+('Sham.John52@live.com', 50, 'JWdVVGj', 'Sham', 'John', 'Customer');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `currentorder`
+--
+ALTER TABLE `currentorder`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Indexes for table `ingredients`
+--
+ALTER TABLE `ingredients`
+  ADD PRIMARY KEY (`ing_id`);
+
+--
+-- Indexes for table `ingredients_required`
+--
+ALTER TABLE `ingredients_required`
+  ADD KEY `ingredient_mapping1` (`ing_id`),
+  ADD KEY `ingredient_mapping2` (`item_id`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`item_id`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`orderid`,`itemid`,`time`);
+
+--
+-- Indexes for table `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`userid`,`item_id`),
+  ADD KEY `rating_mapping1` (`item_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `currentorder`
+--
+ALTER TABLE `currentorder`
+  ADD CONSTRAINT `order_mapping` FOREIGN KEY (`order_id`) REFERENCES `order` (`orderid`);
+
+--
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `user_mapping` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`);
+
+--
+-- Constraints for table `ingredients_required`
+--
+ALTER TABLE `ingredients_required`
+  ADD CONSTRAINT `ingredient_mapping2` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`);
+
+--
+-- Constraints for table `rating`
+--
+ALTER TABLE `rating`
+  ADD CONSTRAINT `rating_mapping2` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
